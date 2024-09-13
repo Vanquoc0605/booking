e61879895a7b87071da0726d94bc3130f49c5744
VanQuoc99 <buivanquocpd@gmail.com>
2024-09-13 11:10:13 +0700

    project_day1

---
+++ js/home.js
@@ -0,0 +1,23 @@
+
+
+document.querySelector('.img-nabar').addEventListener('click', () => {
+    document.querySelector('.click_icon').classList.toggle('hidden')          
+}
+);
+
+document.querySelector('.button_back').addEventListener('click',()=>
+{
+    console.log(document.querySelector('.button_back'));
+    console.log( document.querySelector('.fa-arrow-left').classList.toggle('fa-bars')
+);
+  
+    
+    document.querySelector('.fa-arrow-left').classList.toggle('fa-bars');
+    console.log(document.querySelector('.button_back'));
+})
+
+
+
+
+
+
